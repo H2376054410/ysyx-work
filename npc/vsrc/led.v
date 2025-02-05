ba408e608284c912e1f0a0c9051bb800f9b20564
tracer-ysyx <tracer@ysyx.org>
2025-02-05 18:22:06 +0800

    > 2选1多路选择器 ysyx_22040000 郝伟翔 Linux hwx-com 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:22:06 up 1 day,  1:30,  3 users,  load average: 1.00, 1.32, 1.23

--- npc/vsrc/led.v
+++ npc/vsrc/led.v
@@ -8,13 +8,16 @@ module led(
   reg [31:0] count;
   reg [15:0] led;
   reg [7:0] r_led;
-  always @(posedge clk) begin
-    if (rst) begin led <= 1; count <= 0; end
-    else begin
-      if (count == 0) led <= {led[14:0], led[15]};
-      count <= (count >= 5000000 ? 32'b0 : count + 1);
-    end
+  // always @(posedge clk) begin
+  //   if (rst) begin led <= 1; count <= 0; end
+  //   else begin
+  //     if (count == 0) led <= {led[14:0], led[15]};
+  //     count <= (count >= 5000000 ? 32'b0 : count + 1);
+  //   end
+  // end
+  //0和1为信号输入端，2为信号选择端，输出信号为led[0]
+  always @(*) begin
+    led[0]=sw[2]==0?sw[0]:sw[1];
   end
-
   assign ledr = led;
 endmodule
