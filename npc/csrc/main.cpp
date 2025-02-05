72e053f3da54c174543e81dabe2918b40ab5875f
tracer-ysyx <tracer@ysyx.org>
2025-02-05 17:07:42 +0800

    > 接入NVBOARD ysyx_22040000 郝伟翔 Linux hwx-com 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:07:42 up 1 day, 16 min,  3 users,  load average: 1.89, 1.95, 1.66

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,9 +1,6 @@
-#include <verilated.h>
+#include <nvboard.h>
 #include <Vtop.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <verilated_vcd_c.h>
+
 static TOP_NAME dut;
 
 void nvboard_bind_all_pins(TOP_NAME* top);
@@ -18,18 +15,15 @@ static void reset(int n) {
   while (n -- > 0) single_cycle();
   dut.rst = 0;
 }
-int main(int argc, char** argv) {
-	nvboard_bind_pin( &dut->sw, 8, SW7, SW6, SW5, SW4, SW3, SW2, SW1, SW0);
-  nvboard_bind_pin( &top->ledr, 16, LD15, LD14, LD13, LD12, LD11, LD10, LD9, LD8, LD7, LD6, LD5, LD4, LD3, LD2, LD1, LD0);
+
+int main() {
+  nvboard_bind_all_pins(&dut);
   nvboard_init();
 
   reset(10);
 
-    while (true) {
+  while(1) {
     nvboard_update();
     single_cycle();
-    }
-
-    return 0;
+  }
 }
-
