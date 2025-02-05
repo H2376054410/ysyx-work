a2ff4dbfd71c8bbf06e15f024ce6317a7559f5e5
tracer-ysyx <tracer@ysyx.org>
2025-02-05 17:19:23 +0800

    > 流水灯实例 ysyx_22040000 郝伟翔 Linux hwx-com 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:19:23 up 1 day, 28 min,  3 users,  load average: 2.25, 2.12, 1.91

--- npc/vsrc/led.v
+++ npc/vsrc/led.v
@@ -6,24 +6,15 @@ module led(
   output [15:0] ledr
 );
   reg [31:0] count;
-  reg [7:0] led;
+  reg [15:0] led;
   reg [7:0] r_led;
   always @(posedge clk) begin
     if (rst) begin led <= 1; count <= 0; end
     else begin
-      if (count == 0) led <= {led[6:0], led[7]};
+      if (count == 0) led <= {led[14:0], led[15]};
       count <= (count >= 5000000 ? 32'b0 : count + 1);
     end
   end
-assign r_led[0]=sw[1]^sw[0];
-assign r_led[1]=sw[1]^sw[0];
-assign r_led[2]=sw[1]^sw[0];
-assign r_led[3]=sw[1]^sw[0];
-assign r_led[4]=sw[1]^sw[0];
-assign r_led[5]=sw[1]^sw[0];
-assign r_led[6]=sw[1]^sw[0];
-assign r_led[7]=sw[1]^sw[0];
 
-
-  assign ledr = {led[7:5], led[4:0] ^ btn, r_led};
+  assign ledr = led;
 endmodule
