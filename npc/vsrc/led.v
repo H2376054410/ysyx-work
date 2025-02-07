0ab560a83c115fff150194c978378fca732056b7
tracer-ysyx <tracer@ysyx.org>
2025-02-07 11:17:06 +0800

    > 含有九个状态的状态机 ysyx_22040000 郝伟翔 Linux hwx-com 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:17:06 up  2:50,  1 user,  load average: 1.25, 1.26, 1.07

--- npc/vsrc/led.v
+++ npc/vsrc/led.v
@@ -5,15 +5,50 @@ module led(
   input [7:0] sw,
   output [15:0] ledr
 );
-  reg [3:0] fsm_out; 
-  reg  r_led;
-
-always @(btn[0]) begin
-  if(btn[0]==0)begin
-    fsm_out={fsm_out[2:0],sw[1]};   
+reg [3:0]state,next_state;
+reg out_z;
+    // 状态定义
+    localparam A = 4'b0000,
+               B = 4'b0001,
+               C = 4'b0010,
+               D = 4'b0011,
+               E = 4'b0100,
+               F = 4'b0101,
+               G = 4'b0110,
+               H = 4'b0111,
+               I = 4'b1000;
+  //组合逻辑状态转移规则
+  always @(*) begin
+    case (state)
+        A:next_state=(sw[1])?F:B; 
+        B:next_state= (sw[1])?F:C;
+        C:next_state= (sw[1])?F:D;
+        D:next_state= (sw[1])?F:E;
+        E:next_state= (sw[1])?F:E;
+        F:next_state= (sw[1])?G:B;
+        G:next_state= (sw[1])?H:B;
+        H:next_state= (sw[1])?I:B;
+        I:next_state= (sw[1])?I:B;
+      default: next_state=A;
+    endcase
   end
-end
-  assign r_led=(fsm_out==4'b1111)?1:0;
-  assign ledr = {8'b0000000,fsm_out[3:0],3'b000,r_led};
+  always @(negedge btn[0] or posedge sw[0]) begin
+    if(sw[0]==1)
+    begin
+      state<=A;
+    end
+    else 
+    begin
+      state<=next_state;
+    end
+  end
+
+  always @(*) begin
+    if(state==4'b1111)
+    out_z=1;
+    else
+    out_z=0;
+  end
+  assign ledr = {8'b0000000,state[3:0],3'b000,out_z};
   //assign ledr = {led[7:5], led[4:0] ^ btn, sw};
 endmodule
