bde1122709834de7ef1b9f5ffec3f9801f83bd0e
tracer-ysyx <tracer@ysyx.org>
2025-02-07 10:35:21 +0800

    > 简单状态机 ysyx_22040000 郝伟翔 Linux hwx-com 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:35:21 up  2:08,  1 user,  load average: 1.50, 1.20, 1.00

--- npc/vsrc/led.v
+++ npc/vsrc/led.v
@@ -5,29 +5,15 @@ module led(
   input [7:0] sw,
   output [15:0] ledr
 );
-  reg [31:0] count;
-  reg [15:0] led;
-  reg [7:0] r_led;
-  //8-3优先编码器
-  //led[3:0]为信号输出端，012为信号输出,3为标志位，有1则亮，否则灭
-  //首先保证高位的优先级要大于低位的，所以是i从大变小检测，然后因为得到的i就是位的值，所以只需将i输出即可
-  wire [2:0]choose;
-  wire [7:0]data;
-  reg flag;
-  integer i;
-  assign data=sw[7:0];
-  always @(data) begin
-    choose[2:0]=3'b000;
-    flag=0;
-    for(i=7;i>-1;i=i-1)begin
-      if(data[i]==1)begin
-        choose[2:0]=i[2:0];
-        flag=1;
-        i=-1;
-      end
-    end
-  r_led[3:0]={flag,choose[2:0]};    
+  reg [3:0] fsm_out; 
+  reg  r_led;
+
+always @(btn[0]) begin
+  if(btn[0]==0)begin
+    fsm_out={fsm_out[2:0],sw[1]};   
   end
-  assign ledr = {3'b111,btn[4:0],r_led};
+end
+  assign r_led=(fsm_out==4'b1111)?1:0;
+  assign ledr = {11'b0000000000,fsm_out[3:0],r_led};
   //assign ledr = {led[7:5], led[4:0] ^ btn, sw};
 endmodule
