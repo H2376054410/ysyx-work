7e96cb90917aa231f63c12e330410fbe279e8636
100013812-Hao Weixiang <2376054410@qq.com>
2025-03-08 10:34:21 +0800

    complete add

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -21,17 +21,19 @@
 #define R(i) gpr(i)
 #define Mr vaddr_read
 #define Mw vaddr_write
-
+void print_bits(uint32_t num); 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
-  TYPE_N, TYPE_J,// none
+  TYPE_N, TYPE_J, TYPE_R,
+  TYPE_B,
 };
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immJ() do { *imm = ((((SEXT(BITS(i, 31, 31), 1) << 20)|(SEXT(BITS(i, 19, 12), 8) << 12 )|(SEXT(BITS(i, 20, 20), 1) << 11)|(SEXT(BITS(i, 30, 21), 10) << 1))<<11))>>11;} while(0)
+#define immB() do { *imm = ((((SEXT(BITS(i, 31, 31), 1) << 12)|(BITS(i, 7, 7) << 11 )|(BITS(i, 30, 25) << 5)|(BITS(i, 11, 8) << 1))<<0)>>0);} while(0)
+#define immJ() do { *imm = ((((SEXT(BITS(i, 31, 31), 1) << 20)|(BITS(i, 19, 12) << 12 )|(BITS(i, 20, 20) << 11)|(BITS(i, 30, 21) << 1))<<11))>>11;} while(0)
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst;
   int rs1 = BITS(i, 19, 15);
@@ -42,7 +44,9 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_N:                           break;
+    case TYPE_R: src1R(); src2R();         break;
     case TYPE_J:                   immJ(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
     default: panic("unsupported type = %d", type);
   }
 }
@@ -62,13 +66,18 @@ static int decode_exec(Decode *s) {
 //INSTPAT(模式字符串,                                 指令名称, 指令类型, 指令执行操作);
   INSTPAT_START();
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
-  INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
+  INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) = src1+imm);
   INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
   INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2));
-  INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) = src1+imm);
-  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(rd) = s->pc + 4,s->dnpc=s->pc+imm,printf("the pcc is %x\n",s->pc));
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(rd) = s->pc + 4,s->dnpc=s->pc+imm);
+  INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
   INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, R(rd) = s->pc + 4,s->dnpc=src1+imm);
   INSTPAT("??????? ????? ????? ??? ????? 00100 11", addi   , I, R(rd) = src1+imm);
+  INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw     , I, R(rd) = Mr(src1 + imm, 4));
+  INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add    , R, R(rd) = src1+src2); 
+  INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , R, R(rd) = src1-src2); 
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, s->dnpc=(src1==src2) ? (s->pc+imm) : s->dnpc); 
+  INSTPAT("??????? ????? ????? 001 ????? 11000 11", bne    , B, s->dnpc=(src1!=src2) ? (s->pc+imm) : s->dnpc); 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
@@ -81,4 +90,18 @@ static int decode_exec(Decode *s) {
 int isa_exec_once(Decode *s) {
   s->isa.inst = inst_fetch(&s->snpc, 4);
   return decode_exec(s);
+}
+void print_bits(uint32_t num) {
+  // 从最高位（第31位）开始，逐位输出
+  for (int i = 31; i >= 0; i--) {
+      // 使用位操作检查当前位是否为1
+      uint32_t bit = (num >> i) & 1;
+      printf("%u", bit);
+
+      // 每4位添加一个空格，方便阅读
+      if (i % 4 == 0) {
+          printf(" ");
+      }
+  }
+  printf("\n");
 }
